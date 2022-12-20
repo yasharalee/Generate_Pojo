10398e711224cb8d32e7499dff1eb146f4ffe954
yasharalee <77319904+yasharalee@users.noreply.github.com>
2022-12-19 23:20:31 -0800

    generating pojo files

--- src/test/java/com/cydeo/BurnRunner.java
+++ src/test/java/com/cydeo/BurnRunner.java
@@ -9,9 +9,11 @@ import static com.cydeo.BurnToClass.*;
 public class BurnRunner {
     public static void main(String[] args) {
 
-
         JsonPath jsonPath = sendThese("http://54.237.233.250:1000/ords/hr/countries");
         Map<String, Object> jsToMap = jsonPath.getObject("", Map.class);
+
+        //this method will generate and add POJO files under "cydeo" package, dynamically
+        //it is able to create List of custom Classes
         makePojo(jsToMap, "hr");
 
 
